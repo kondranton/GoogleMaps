d56702b44fd8c330e1aa58e5dd21f3d7470b87ad
Anton <akondranton@gmail.com>
2019-07-07 23:15:35 +0300

    Initial commit

---
+++ GoogleMapsWG/GoogleMapsWG.h
@@ -0,0 +1,19 @@
+//
+//  GoogleMapsWG.h
+//  GoogleMapsWG
+//
+//  Created by Anton Kondrashov on 07/07/2019.
+//  Copyright © 2019 Anton Kondrashov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleMapsWG.
+FOUNDATION_EXPORT double GoogleMapsWGVersionNumber;
+
+//! Project version string for GoogleMapsWG.
+FOUNDATION_EXPORT const unsigned char GoogleMapsWGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleMapsWG/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GoogleMapsWG' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GoogleMapsWG
+
+  target 'GoogleMapsWGTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'GoogleMaps', '2.7.0'
+  end
+
+end
